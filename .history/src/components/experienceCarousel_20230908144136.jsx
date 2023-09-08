b18bad0f659f24422eed27dced206087fa4917b1
Thomas Musial <th896493@dal.ca>
2023-09-08 14:52:30 -0300

    carousel rage

---
+++ .history/src/components/experienceCarousel_20230908144136.jsx
@@ -0,0 +1,70 @@
+
+
+const experiencesData = [
+  {
+    id: 1,
+    title: 'Front-end Developer',
+    company: 'ABC Company',
+    date: 'June 2020 - Present',
+    description:
+      'Developed and maintained user interfaces like a real one',
+    technologies: ['React.js', 'HTML', 'CSS', 'JavaScript'],
+  },
+  {
+    id: 2,
+    title: 'real one',
+    company: 'reality',
+    date: 'my whole life',
+    description: 'i have always given it my best and i will continue to do so to reach my max potential!',
+    technologies: ['c','react.js','blender','css','javascript']
+  }
+  // Add more experience objects here...
+];
+
+import React, { useState, useEffect,useRef } from 'react';
+import Experience from './Experience';
+
+
+
+const ExperienceCarousel = () => {
+ const [currentIndex,setCurrentIndex] = useState(0)
+
+ const sliderRef = useRef(null)
+  const nextExperience = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % experiencesData.length);
+  };
+
+  const prevExperience = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === 0 ? experiencesData.length - 1 : prevIndex - 1
+    );
+  };
+
+  const handleMouseEnter = () => {
+    setIsHovered(true)
+    console.log("hover")
+  }
+
+  return (
+    <div
+      className="experience-carousel"
+     
+    >
+      <div className='experience-card'>
+      {/* <Experience
+        title={experiencesData[currentIndex].title}
+        company={experiencesData[currentIndex].company}
+        date={experiencesData[currentIndex].date}
+        description={experiencesData[currentIndex].description}
+        technologies={experiencesData[currentIndex].technologies}
+        
+      /> */}
+      </div>
+      <div className='experience-buttons'>
+       
+      </div>
+    </div>
+  );
+};
+
+export default ExperienceCarousel;
---
+++ .history/src/components/experience_20230908143415.jsx
@@ -0,0 +1,47 @@
+import React,{ useState,useRef } from 'react';
+import vegeta from '../images/vegeta-battle.png';
+
+const Experience = (props) => {
+    const [isHovered, setIsHovered] = useState(false)
+  const { title, company, date, description, technologies,  } = props;
+
+
+
+
+const handleMouseEnter = () => {
+    setIsHovered(true)
+    console.log("we have hovering")
+}
+
+const handleMouseLeave = () => {
+    setHovered(false)
+}
+    
+  return (
+    <div className="experience">
+      <div className="experience-content">
+        <div className='details'>
+      
+        </div>
+        <div className='experience-description'
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}>
+          <div className='content-container'>
+
+            <h2>Websites</h2>
+
+            <img src={vegeta}
+            />
+            </div>
+        
+        <p >
+            {description}</p>
+            
+      </div>
+      </div>
+     
+    </div>
+  );
+};
+
+export default Experience;
